3fa45b2bc5b8282a47d0741eb3cec6dfbf8bc5cf
Gabriel Carneiro <gabriel.carneiro97@live.com>
2019-10-04 10:48:25 -0300

    boilerplate ready

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'import/no-dynamic-require': 0,
+    'import/no-extraneous-dependencies': ['error', {'devDependencies': true, 'optionalDependencies': false, 'peerDependencies': false}],
+  },
+};
---
+++ public/electron.js
@@ -0,0 +1,55 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+require('electron-reload')(path.join(__dirname));
+
+let mainWindow;
+let backgroundWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  backgroundWindow = new BrowserWindow({
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  backgroundWindow.loadFile(path.join(__dirname, '/backend/process.html'));
+
+  if (isDev) {
+    // Open the DevTools.
+    // mainWindow.addDevToolsExtension('<location to your react chrome extension>');
+    mainWindow.webContents.openDevTools({ mode: 'detach' });
+    backgroundWindow.webContents.openDevTools({ mode: 'detach' });
+  }
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+    backgroundWindow.destroy();
+  });
+
+  backgroundWindow.on('closed', () => {
+    backgroundWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
